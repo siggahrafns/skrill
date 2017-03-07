8be50515808078ff3c3b122652ff65076cb62daf
siggahrafns <sh@etactica.com>
2017-03-07 17:13:30 +0000

    Kitten lands on grass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,12 @@ import {ScrollSpyService} from "ng2-scrollspy/dist/index";
   template: require('./home.component.html')
 })
 export class HomeComponent  implements AfterViewInit {
-  private pos = -200;
+  private kittyStill = false;
+
+  private kittyPosTop = -200;
+  private kittyPosLeft = 0;
+  private grassPos = -2000;
+  private grassOpacity = 0;
 
   constructor(private scrollSpyService: ScrollSpyService) {}
 
@@ -16,11 +21,30 @@ export class HomeComponent  implements AfterViewInit {
       //console.log('ScrollSpy::test: ', e);
       if(e ){
         let scrollTop = e.srcElement.scrollingElement.scrollTop;
+        console.log(scrollTop);
+
         if(scrollTop > 20) {
-          this.pos = scrollTop*2;
+          this.kittyPosTop = scrollTop + Math.floor(scrollTop/2);
+          this.kittyPosLeft = scrollTop - Math.floor(scrollTop/2);
+        }
+        if (scrollTop < 20) {
+          this.kittyPosTop = -200;
+          this.kittyPosLeft = 0;
+        }
+
+        if(scrollTop >= 500) {
+          this.grassPos = 0;
+          this.grassOpacity = 1;
+        }
+        if(scrollTop >= 900) {
+          this.kittyPosTop =1700;
+          this.kittyPosLeft = 500;
+          this.kittyStill = true;
         }
         else {
-          this.pos = -200;
+          this.kittyStill = false;
+          this.grassOpacity = 0;
+          this.grassPos = -2000;
         }
 
       }
