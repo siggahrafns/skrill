b7e02376644306c6c4539c062d550c5655c27bd0
siggahrafns <sh@etactica.com>
2017-03-10 16:22:57 +0000

    Added better animation and some SKRILL

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,29 +7,54 @@ import {ScrollSpyService} from "ng2-scrollspy/dist/index";
   template: require('./home.component.html')
 })
 export class HomeComponent  implements AfterViewInit {
-  private kittyStill = false;
+  private startTransition: boolean = false;
+  private secondTransition: boolean = false;
 
-  private kittyPosTop = -200;
-  private kittyPosLeft = 0;
+  private moveLeft = false;
+
+  private kittyStill = false;
+  private kittyPosTop = 40;
+  private kittyPosLeft = 200;
+  private kittyOpacity = 0;
+
   private grassPos = -2000;
   private grassOpacity = 0;
+
+  private welcomePosLeft = -50;
+  private welcomeOpacity = 0;
 
   constructor(private scrollSpyService: ScrollSpyService) {}
 
   ngAfterViewInit() {
+    let that = this;
+    setTimeout(function() {
+      that.startTransition = true;
+      setTimeout(function() {
+        console.log('hallo')
+        that.secondTransition = true;
+      }, 2500);
+    }, 1000);
+
+
+
     this.scrollSpyService.getObservable('window').subscribe((e: any) => {
-      //console.log('ScrollSpy::test: ', e);
       if(e ){
-        let scrollTop = e.srcElement.scrollingElement.scrollTop;
+        /*let scrollTop = e.srcElement.scrollingElement.scrollTop;
         console.log(scrollTop);
 
-        if(scrollTop > 20) {
+        if(scrollTop > 53 && scrollTop <= 300) {
           this.kittyPosTop = scrollTop + Math.floor(scrollTop/2);
-          this.kittyPosLeft = scrollTop - Math.floor(scrollTop/2);
+          this.kittyPosLeft += 1;
+          this.kittyOpacity = 1;
         }
-        if (scrollTop < 20) {
-          this.kittyPosTop = -200;
-          this.kittyPosLeft = 0;
+        if (scrollTop <= 53) {
+          this.kittyPosTop = 40;
+          this.kittyPosLeft = 200;
+          this.kittyOpacity = 0;
+        }
+        if (scrollTop >= 300) {
+          this.kittyPosTop  = scrollTop + Math.floor(scrollTop/2);
+          this.kittyPosLeft = scrollTop - Math.floor(scrollTop/3);
         }
 
         if(scrollTop >= 500) {
@@ -37,16 +62,22 @@ export class HomeComponent  implements AfterViewInit {
           this.grassOpacity = 1;
         }
         if(scrollTop >= 900) {
+          this.moveLeft = true;
           this.kittyPosTop =1700;
-          this.kittyPosLeft = 500;
+          this.kittyPosLeft = 9000;
           this.kittyStill = true;
+          this.welcomePosLeft = 50;
+          this.welcomeOpacity = 1;
+
         }
         else {
+          this.moveLeft = false;
           this.kittyStill = false;
           this.grassOpacity = 0;
           this.grassPos = -2000;
-        }
-
+          this.welcomePosLeft = -50;
+          this.welcomeOpacity = 0;
+        }*/
       }
     });
   }
